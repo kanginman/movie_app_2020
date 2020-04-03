ca86f139a2a6fe01549d895e093eb3120c46071f
kanginman <kanginman@gmail.com>
2020-04-03 12:06:48 +0900

    Updates

---
+++ precache-manifest.f90659420fccf456f3273360f7b493fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de0927d04cb6d5f0c30ebadc06bb10f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "280ab0e6cefc9e8592da",
+    "url": "/movie_app_2020/static/css/main.53c29a87.chunk.css"
+  },
+  {
+    "revision": "486a33f18233073eb844",
+    "url": "/movie_app_2020/static/js/2.56f08216.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56f08216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280ab0e6cefc9e8592da",
+    "url": "/movie_app_2020/static/js/main.a4f0a334.chunk.js"
+  },
+  {
+    "revision": "8b67d7ccc50a17c9e597",
+    "url": "/movie_app_2020/static/js/runtime-main.aaeb0f22.js"
+  }
+]);
